689240db7cc6f58f9f68d69902062104108e0ce8
Unmesa <dey1011unmesa@gmail.com>
2023-07-10 16:48:04 +0530

    Initial commit

---
+++ src/component/Sidebaar/Sidebar.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+import './Sidebar.css';
+
+import { MdAddCircle } from "react-icons/md";
+
+
+
+
+
+function Sidebar(props) {
+  const colors=["#fe9b72","#fec971","#00d4fe","#b693fd","#e4ee91"];
+  const[listOpen,setListOpen]=useState(false);
+  
+  return (
+    <div className='sidebar'>
+    
+      <MdAddCircle className='icon' onClick={()=>setListOpen(!listOpen)} />
+      <ul className={`sidebar_list ${listOpen ? "sidebar_list_active" :""}`}>
+      {
+        colors.map((item,index) =>( 
+        <li 
+        key={index}
+        className='sidebar_list_item' 
+        style={{backgroundColor:item}}
+        onClick={()=>props.addNote(item)}
+         />
+      ))
+      }
+       
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar
